24e374908be35632f00fd48bc9b5f26fbbb528f5
WiraBhakti <wiranamaku@gmail.com>
2019-03-17 21:34:17 +0700

    new ipynb file

--- insertionsort.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-
-def insertion_sort(arr):
-    selected_num = arr[0]
-    for i in range(len(arr)):
-        selected_num = arr[i+1]
-        for i in arr[:selected_num]:
-
